fc3bea5a90606476e8cccf7cb4bd838031dd56d8
A.J. May <aj7may@gmail.com>
2019-11-18 19:08:32 -0500

    bump version to 1.4.1

--- Formula/dotdocker.rb
+++ Formula/dotdocker.rb
@@ -1,8 +1,8 @@
 class Dotdocker < Formula
   desc "Setup a docker development environment with host based routing"
   homepage "https://github.com/aj-may/dotdocker/"
-  url "https://github.com/aj-may/dotdocker/releases/download/v1.3.4/dotdocker-macos"
-  sha256 "fc0cadd75e34dde0ec5a1c6c51f9af38819db2dead849c1f066b5ed37ab64151"
+  url "https://github.com/aj-may/dotdocker/releases/download/v1.4.1/dotdocker-macos"
+  sha256 "1ce059c4e91091c53c5de3144587a1d0802b909fa8fea89cac1c5bd7f3eaec2e"
 
   def install
     File.rename "dotdocker-macos", "dotdocker"
